d1daada48ee0bcb209a9950a32aad1979bdf2009
emohamedd <a.elgaouej@gmail.com>
2023-11-16 11:26:16 +0100

    new update

--- shell.h
+++ shell.h
@@ -14,10 +14,10 @@
 
 /**
  * struct s_shell - shell struct
- * @argv: arguments vector
- * @env: environement variables
- * @line: the command line input
- * @tokens: command tokens after splitting
+ * @argv: arguments
+ * @env: environement
+ * @line: the command line
+ * @tokens: command tokens
  * @error_counter: number of the error
  * @status: the status code
  * Description: shell struct
@@ -35,28 +35,28 @@ int error_counter;
 
 extern char **environ;
 
-char	*ft_strjoin(char *s1, char *s2);
 int get_last_exit(int action, int status);
+char	*ft_strjoin(char *s1, char *s2);
 void	*_memcpy(void *dst, const void *src, size_t n);
 char	*get_next_line(int fd);
 char **ft_split(char *s, char *delimiters);
 char	*ft_substr(char *s, int start, int len);
 char *_strchr(const char *s, int c);
-char *_strcpy(char *dest, char *src);
 int get_last_exit(int action, int status);
 int _strlen(char *str);
 int _strncmp(char *str1, char *str2, int n);
 char *_strdup(char *str);
 void sigintHandler(int sig_num);
 char *_strcat(char *dest, char *src);
-char *make_path(char *path, char *cmd);
+char *_strcpy(char *dest, char *src);
 char *_getenv(char *name);
-int execute(t_shell *shell);
+char *make_path(char *path, char *cmd);
 char *get_path(char *cmd);
-int builtins(t_shell *shell);
+int execute(t_shell *shell);
 void free_tokens(char **ptr);
-void cut_string(char *str);
+int builtins(t_shell *shell);
 int _atoi(char *str);
+void cut_string(char *str);
 char *_itoa(int num);
 int _isnumber(char *str);
 void _putchar(int c);
